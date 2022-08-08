2a4b37ea40bef64b5ce06719e253f8c8b707f6c4
ARMAN-DREAMER <arman.kangavari1997@gmail.com>
2022-08-08 14:21:39 -0700

    lool

--- ui.js
+++ ui.js
@@ -4,3 +4,13 @@ alert("gulu gulu");
 alert("hi");
 console.log("heloooo");
 alert("gulu gulu");
+/*le[m2oooooooooooooo[2'
+e2lfmo2m[fm2epmfe2
+em2fk2lefp2mfe2
+fp2]ekfp2kf]pe2f
+ef,2emf]o2fm'2e,f
+efm]pe2mfp2ef
+2f2pef2pmf
+2f2pe]pfp2efjp2kfp2kk
+2pejp3fp32kpf23p23k23kfp
+2k32f,,w]]]*/
